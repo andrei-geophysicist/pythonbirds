e8b1fcf58853b298b2062367c36771069c89cd6e
andrei <andrei.jval@gmail.com>
2020-06-15 10:45:09 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self): # método(objeto)
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) # O parâmetro p está explícito
+    print(id(p)) # imprime a id do do objeto self
+    print(p.cumprimentar()) # O parâmetro p está implícito
